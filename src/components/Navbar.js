393e709df7c8f46f155c10662453fa892388ccd7
Kseniia Khodyreva <melidios@Kseniias-MacBook-Pro.local>
2019-01-09 10:03:48 -0500

    Edit navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,112 @@
-import React, { Component } from 'react';
+import React from 'react';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
 
-class Navbar extends Component {
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  bar: {
+    background: 'linear-gradient(45deg, #b388ff 30%, #7e57c2 90%)',
+    color: 'white',
+    fontWeight: 'bold',
+  },
+};
+
+
+class Navbar extends React.Component {
+  state = {
+    auth: true,
+    anchorEl: null,
+  };
+
+  handleChange = event => {
+    this.setState({ auth: event.target.checked });
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleCloseProfile = () => {
+    this.setState({ anchorEl: null })
+  };
+  handleCloseAccount = () => {
+    this.setState({ anchorEl: null })
+  };
+  handleCloseLogOut = () => {
+    this.setState({ anchorEl: null })
+  };
+
   render() {
+    const { classes } = this.props;
+    const { auth, anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
     return (
-      <div>
-        This is navbar
+      <div className={classes.root}>
+        <AppBar position="fixed" className={classes.bar}>
+          <Toolbar>
+            <IconButton className={classes.menuButton}            
+                  aria-owns={open ? 'menu-appbar' : undefined}
+                  aria-haspopup="true"
+                  onClick={this.handleMenu}
+                  color="inherit"
+            >
+              <MenuIcon />
+           
+            </IconButton>
+            <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleCloseProfile}>Projects</MenuItem>
+                  <MenuItem onClick={this.handleCloseAccount}>Bio</MenuItem>
+                  <MenuItem onClick={this.handleCloseLogOut} >Contacts</MenuItem>
+                </Menu>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Home
+            </Typography>
+            {auth && (
+              <div>
+                <IconButton
+                  
+                >
+                  <AccountCircle />
+                </IconButton>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
