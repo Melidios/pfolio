7554f8b3dd1586597dd14bff8f7eb34e8265cdc3
Melidios One <melidiosone@gmail.com>
2019-01-18 14:32:12 -0500

    styling components

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -9,6 +9,7 @@ const styles = theme => ({
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
     paddingBottom: theme.spacing.unit * 2,
+    margin: '10px 0 10px 0',
   },
 });
 
